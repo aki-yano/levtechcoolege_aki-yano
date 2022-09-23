7d58be1469a647b698c76e57aa9888259f376213
aki-yano <sball5341@gmail.com>
2022-09-23 19:01:52 +0900

    ページネーション

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,16 +5,19 @@
         <title>Blog</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body class="antialiased">
        <h1>Blog Name</h1>
        <div class='posts'>
-           <div class='post'>
-               <h2 class='title'>Title1</h2></h2>
-               <p class='body'>This si a simple  body</p>
-           </div>
-        </div>
+           @foreach($posts as $post)
+               <div class='post'>
+                   <h2 class='title'>{{$post->title}}</h2>
+                   <p class='body'>{{$post->body}}</p>
+               </div>
+           @endforeach
+       </div>
+       <div class='paginate'>{{ $posts->links()}}</div>
     </body>
 </html>
